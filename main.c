ce306cd9a9f99c82408b9e03c9e5c422bd0c6c48
wispl <wispl.8qbkk@slmail.me>
2024-06-02 18:09:35 -0400

    feat: take command argument as filename

--- main.c
+++ main.c
@@ -3,13 +3,20 @@
 
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	struct pgn *pgn = pgn_read("test.pgn");
+	if (argc > 3 || argc < 2) {
+		printf("[Error] No arguments or too many arguments were passed");
+		return 0;
+	}
+
+	struct pgn *pgn = pgn_read(argv[1]);
+
 	struct tag *tag;
 	list_for_each_entry(tag, &pgn->tags, node) {
 		printf("%s: %s\n", tag->type, tag->description);
 	}
+
 	struct move *move;
 	list_for_each_entry(move, &pgn->moves, node) {
 		printf("%s %s\n", move->white, move->black);
