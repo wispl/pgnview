676b57eeacfea0035ea20f3c43da8d5151edda19
wispl <wispl.8qbkk@slmail.me>
2024-06-09 01:20:24 -0400

    main: fix code

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
+#include "array.h"
 #include "bitboard.h"
-#include "list.h"
 #include "movegen.h"
 #include "parser.h"
 
@@ -16,47 +16,47 @@ int main(int argc, char **argv)
 	struct pgn pgn;
 	pgn_read(&pgn, argv[1]);
 
-	struct pgn_tag *tag;
-	list_for_each_entry(tag, &pgn.tags, node) {
-		printf("%s: %s\n", tag->name, tag->desc);
+	for (int i = 0; i < pgn.tags.len; ++i) {
+		printf("%s ", array_get(&pgn.tags, struct pgn_tag, i).name);
+		printf("%s\n", array_get(&pgn.tags, struct pgn_tag, i).desc);
 	}
-	struct pgn_move *move;
-	list_for_each_entry(move, &pgn.moves, node) {
-		printf("%s ", move->text);
+	printf("\n");
+	for (int i = 0; i < pgn.moves.len; ++i) {
+		printf("%s ", array_get(&pgn.moves, struct pgn_move, i).text);
 	}
 	printf("%s\n", pgn.result);
 
-	pgn_free(&pgn);
 	printf("\n");
+	
+	pgn_free(&pgn);
 
 	printf("============= Testing Movegen =============\n");
 	init_lineattacks_table();
 	struct board board;
 	board_init(&board);
 
-	struct movelist movelist;
+	struct movebuf buf;
 	struct movegenc movegenc = {
 		.piece = PAWN,
 		.color = WHITE,
-		.movetype = QUIET
+		.movetype = QUIET,
+		.target   = ~(0ULL),
 	};
-	generate_moves(&board, &movelist, &movegenc);
+	generate_moves(&board, &buf, &movegenc);
 
-	for (int i = 0; i < movelist.len; ++i) {
-		struct move move = movelist.moves[i];
+	for (int i = 0; i < buf.len; ++i) {
+		struct move move = buf.moves[i];
 		print_square(move.from);
 		printf("->");
 		print_square(move.to);
 		printf(" ");
 	}
-	printf("\n");
-
-	printf("\n");
+	printf("\n\n");
 
 	printf("============= Testing Board =============\n");
-	board_move(&board, &movelist.moves[2]);
+	board_move(&board, &buf.moves[2]);
 	struct move knight_move = {
-		.type = QUIET,
+		.movetype = QUIET,
 		.from = g8,
 		.to   = f6
 	};
