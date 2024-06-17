6c5a33c429ee1b95318d8eda735d515da4585e04
wispl <wispl.8qbkk@slmail.me>
2024-06-17 13:40:37 -0400

    tests: move test to dedicated directory, add stub to main.c

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
 CC = cc
 CFLAGS += -Wextra -Wall -Wdouble-promotion -g3 -fsanitize=undefined,address
 LDFLAGS += -fsanitize=undefined,address
-OBJECTS = main.o parser.o movegen.o pgn_movelist.o bitboard.o board.o
+OBJECTS = parser.o movegen.o pgn_movelist.o bitboard.o board.o termbox2.o
 
-pgncat: $(OBJECTS)
-	$(CC) -o pgncat $(OBJECTS) $(LDFLAGS)
-main.o: parser.h pgn_movelist.h movegen.h bitboard.h array.h move.h
+pgncat: $(OBJECTS) main.c
+	$(CC) -o pgncat main.c $(OBJECTS) $(LDFLAGS)
+
+# TODO: better testing
+test: $(OBJECTS) tests/test.c
+	$(CC) -o test tests/test.c $(OBJECTS) $(LDFLAGS)
+# test.o: parser.h pgn_movelist.h movegen.h bitboard.h array.h move.h
+
 parser.o: parser.h array.h
 pgn_movelist.o: pgn_movelist.h array.h bitboard.h movegen.h move.h
 movegen.o: movegen.h board.h array.h move.h
 board.o: board.h bitboard.h move.h
 bitboard.o: bitboard.h
+termbox2.o: termbox2.h
 clean:
-	rm -f pgncat $(OBJECTS)
+	rm -f pgncat test $(OBJECTS)
--- main.c
+++ main.c
@@ -1,115 +1,65 @@
-#include "array.h"
-#include "bitboard.h"
-#include "movegen.h"
-#include "pgn_movelist.h"
-#include "parser.h"
+#include "termbox2.h"
 
 #include <stdio.h>
+#include <stdbool.h>
 
-static void test_parser()
+#define CELLW  5
+#define CELLH  2
+
+#define SQUARES 64
+#define ROWS    (8 * CELLW)
+#define COLS    (8 * CELLH)
+
+#define LEFTX  (tb_width() / 2) - (ROWS / 2)
+#define LEFTY  4
+
+#define RIGHTX (LEFTX + ROWS / 2)
+#define RIGHTY (LEFTY + COLS)
+
+// static struct board board;
+
+void draw_square(int x, int y, uintattr_t fg, uintattr_t bg)
 {
-	printf("============= Testing Parser =============\n");
-	struct pgn pgn;
-
-	pgn_read(&pgn, "test.pgn");
-	for (int i = 0; i < pgn.tags.len; ++i) {
-		struct pgn_tag tag = array_get(&pgn.tags, i);
-		printf("%s \"%s\"\n", tag.name, tag.desc);
-	}
-	printf("\n");
-	for (int i = 0; i < pgn.moves.len; ++i) {
-		printf("%s ", array_get(&pgn.moves, i).text);
-	}
-	printf("%s\n\n", pgn.result);
-
-	pgn_free(&pgn);
-	pgn_read(&pgn, "test2.pgn");
-	for (int i = 0; i < pgn.tags.len; ++i) {
-		struct pgn_tag tag = array_get(&pgn.tags, i);
-		printf("%s \"%s\"\n", tag.name, tag.desc);
-	}
-	printf("\n");
-	for (int i = 0; i < pgn.moves.len; ++i) {
-		printf("%s ", array_get(&pgn.moves, i).text);
-	}
-	printf("%s\n", pgn.result);
-	pgn_free(&pgn);
+	tb_print(x, y,     bg, fg, "▄▄▄▄▄");
+	tb_print(x, y + 1, fg, bg, "  N  ");
+	tb_print(x, y + 2, bg,  0, "▀▀▀▀▀");
 }
 
-static void test_movegen()
+void draw_board()
 {
-	printf("============= Testing Movegen =============\n");
-	init_lineattacks_table();
-	struct movelist ARRAY(moves);
-	struct board board;
-	board_init(&board);
+	int x = LEFTX;
+	int y = LEFTY;
+	int shift = 0;
+	for (int rank = 7; rank >= 0; --rank) {
+		for (int file = 0; file < 8; ++file) {
+			if ((file + shift) & 1) {
+				draw_square(x, y, TB_GREEN, TB_BLACK);
+			} else {
+				draw_square(x, y, TB_BLACK, TB_GREEN);
+			}
+			x += CELLW;
+		}
+		shift += 1;
 
-	struct movegenc movegenc = {
-		.piece = PAWN,
-		.color = WHITE,
-		.movetype = QUIET,
-		.target   = ~(0ULL),
-	};
-	generate_moves(&board, &moves, &movegenc);
-	for (int i = 0; i < moves.len; ++i) {
-		struct move move = array_get(&moves, i);
-		print_square(move.from);
-		printf("->");
-		print_square(move.to);
-		printf(" ");
+		x = LEFTX;
+		y += CELLH;
 	}
-	printf("\n\n");
-	array_free(&moves);
-}
-
-static void test_board()
-{
-	printf("============= Testing Board =============\n");
-	struct board board;
-	board_init(&board);
-
-	struct move m0 = {
-		.movetype = QUIET,
-		.from = e2,
-		.to   = e4
-	};
-	struct move m1 = {
-		.movetype = QUIET,
-		.from = g8,
-		.to   = f6
-	};
-	board_move(&board, &m0);
-	board_move(&board, &m1);
-
-	board_print(&board);
-}
-
-static void test_pgn_movelist()
-{
-	printf("============= Testing PGN Movelist =============\n");
-	struct pgn pgn;
-	struct movelist ARRAY(moves);
-
-	pgn_read(&pgn, "test.pgn");
-	pgn_movelist(&pgn.moves, &moves);
-
-	for (int i = 0; i < moves.len; ++i) {
-		struct move move = array_get(&moves, i);
-		print_square(move.from);
-		printf("->");
-		print_square(move.to);
-		printf(" ");
-	}
-	printf("\n");
-	array_free(&moves);
-	pgn_free(&pgn);
 }
 
 int main(int argc, char **argv)
 {
-	test_parser();
-	test_movegen();
-	test_board();
-	test_pgn_movelist();
-	return 1;
+	// board_init(&board);
+
+	struct tb_event event;
+	int y = 0;
+
+	tb_init();
+	tb_hide_cursor();
+
+	draw_board();
+	tb_present();
+
+	tb_poll_event(&event);
+
+	tb_shutdown();
 }
