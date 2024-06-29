b6da02ede79f0c23973b3d6547dd9cadd36304a5
wispl <wispl.8qbkk@slmail.me>
2024-06-29 18:14:27 -0400

    pgn_ext: do not modify pgn when calling pgn_to_moves

--- pgn_ext.c
+++ pgn_ext.c
@@ -21,7 +21,7 @@ struct moveinfo {
 	int hint;
 };
 
-static enum piece chrtopiece(char c)
+static enum piece chrtopiece(const char c)
 {
 	switch (c) {
 	case 'K': return KING;
@@ -33,17 +33,17 @@ static enum piece chrtopiece(char c)
 	return PAWN;
 }
 
-static inline int ranktoi(char c)
+static inline int ranktoi(const char c)
 {
 	return (c - '1') * 8;
 }
 
-static inline int filetoi(char c)
+static inline int filetoi(const char c)
 {
 	return c - 'a';
 }
 
-static inline int santoi(char *san)
+static inline int santoi(const char *san)
 {
 	return filetoi(san[0]) + ranktoi(san[1]);
 }
@@ -57,30 +57,31 @@ static inline int santoi(char *san)
 // QUIET: 	e4, Nf6
 // CAPTURES:	exd4->ed4, Nxf6->Nf6
 // PROMOTIONS:  ed4=Q->ed4
-static int extract_san(char *text, int len, struct movegenc *conf)
+static int extract_san(const char *from, const char *to, struct movegenc *conf)
 {
-	assert(len >= 2 && len <= 5);
+	int len = to - from;
+	assert(len >= 0 && len <= 4);
 
-	// piece type is first char, lowercase means it is a pawn
-	conf->piece    = chrtopiece(text[0]);
-	// destination square occurs two chars from end
-	conf->target   = square_bb(santoi(&text[len - 2]));
+	// skip the 'x'
+	if (conf->type == CAPTURE || conf->type == PROMO_CAPTURE)
+		++to;
+
+	// first character indicates the piece type, or a pawn if lowercase
+	conf->piece  = chrtopiece(from[0]);
+	conf->target = square_bb(santoi(to));
 
 	switch (len) {
-	// Pawn moves: e5, d5 -> e5, d5, these don't need disambiguation
-	case 2: return -1;
-	// Non-ambiguous moves: Nf6, Bg6 -> Nf6, Bg6
-	// isupper is special case for pawn captures: exd4 -> (e)d4
-	// otherwise it is just a regular move/capture with no ambiguation
-	case 3: return isupper(text[0]) ? -1 : filetoi(text[0]);
-	// Ambiguous moves: Ngf6, R2f2 -> N(g)f6, R(2)f2
-	// These are moves which require a rank or file to indiciate a piece,
-	// such as when two rooks are on the same rank
-	case 4: return isalpha(text[1]) ? filetoi(text[1]) : ranktoi(text[1]);
-	// Ex Ambiguous moves: Ng8f6 -> N(g8)f6
-	// These not very common but may occur when you have, for
-	// example, three rooks attacking one square
-	case 5: return santoi(&text[1]);
+	// Pawn moves: e5, d5, these never need disambiguation
+	case 0: return -1;
+	// Non-ambiguous moves: (N)f6, (B)g6, (e)xd4
+	case 1: return -1;
+	// Ambiguous moves: (Ng)f6, (R2)f2
+	// These moves require either a rank or file to identify a piece, such
+	// as when two rooks are on the same rank.
+	case 2: return isalpha(from[1]) ? filetoi(from[1]) : ranktoi(from[1]);
+	// Extra Ambiguous moves: (Ng8)f6
+	// Moves which require a square to identify a piece. Not very common.
+	case 3: return santoi(&from[1]);
 	}
 	return -1;
 }
@@ -91,6 +92,7 @@ static int extract_san(char *text, int len, struct movegenc *conf)
 static void get_moveinfo(char *text, enum color color, struct moveinfo *info)
 {
 	info->conf.color = color;
+	info->conf.type = QUIET;
 
 	bool short_castle = (strcmp(text, "O-O") == 0);
 	bool long_castle  = (strcmp(text, "O-O-O") == 0);
@@ -107,35 +109,27 @@ static void get_moveinfo(char *text, enum color color, struct moveinfo *info)
 
 	int len = strlen(text);
 
-	// checks and mates
+	// checks and mates, ignore for now, no usable information
 	char end = text[len - 1];
 	if (end == '+' || end == '#')
 		--len;
 
-	// captures and promotions
-	char* x_start  = strchr(text, 'x');
-	char* eq_start = strchr(text, '=');
-
+	// promotions, for example, fxg8(=Q)
+	char *eq_start = strchr(text, '=');
 	if (eq_start) {
 		len -= 2;
 		info->conf.type = PROMOTION;
 		info->promo_piece = chrtopiece(text[len]);
 	}
+
+	char *to = &text[len - 2];
+	char *x_start  = strchr(text, 'x');
 	if (x_start) {
-		len -= 1;
-		// get rid of the 'x', making it just a regular quiet move
-		// exd5 -> ed5, Nxf6 -> Nf6
-		int i = x_start - text;
-		text[i]     = text[i + 1];
-		text[i + 1] = text[i + 2];
-		text[i + 2] = '\0';
-		info->conf.type = CAPTURE;
-		
-	} else {
-		info->conf.type = QUIET;
+		to = x_start;
+		info->conf.type = (eq_start) ? PROMO_CAPTURE : CAPTURE;
 	}
 
-	info->hint = extract_san(text, len, &info->conf);
+	info->hint = extract_san(text, to, &info->conf);
 }
 
 // Checks if a file, a rank, or both file or rank matches 'from' based on
